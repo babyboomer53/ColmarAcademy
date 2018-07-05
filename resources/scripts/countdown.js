3465264a6dd25b44f348c7070fabe839c8f95c6f
babyboomer53 <edgar.cole@san.rr.com>
2018-07-04 21:36:35 -0700

    More preliminary work.
    Additional refinements to the layout. Added links to external JavaScript libraries. Started formulating class names.

---
+++ resources/scripts/countdown.js
@@ -0,0 +1,7 @@
+$('[data-countdown]').each(function () {
+    var $this = $(this),
+        finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function (event) {
+        $this.html(event.strftime('%D days %Hh %Mm %Ss'));
+    });
+});
