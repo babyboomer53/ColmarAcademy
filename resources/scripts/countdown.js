90af77f922479c567f79a64ed6c82caab77c0317
babyboomer53 <edgar.cole@san.rr.com>
2018-07-10 12:24:32 -0700

    All elements are on the page.
    I've managed to finish a rough layout in which all of the elements have been placed on the page. There's still much work to be done. Most of the elements are overflowing their containers. I suspect that's due to the padding used to position elements relative to each other.

--- resources/scripts/countdown.js
+++ resources/scripts/countdown.js
@@ -2,6 +2,6 @@ $('[data-countdown]').each(function () {
     var $this = $(this),
         finalDate = $(this).data('countdown');
     $this.countdown(finalDate, function (event) {
-        $this.html(event.strftime('%-D days %-H hours %-M minutes and %S seconds'));
+        $this.html(event.strftime('%-D days %-H hours %-M minutes and %S seconds Left'));
     });
 });
